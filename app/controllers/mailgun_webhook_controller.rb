b7d5d46caf6471e07a9b6152e51e032419e1bdde
Rahul <rahul.sharma.sai@gmail.com>
2017-05-21 13:25:22 +0530

    added mailgun webhooks

--- app/controllers/mailgun_webhook_controller.rb
+++ app/controllers/mailgun_webhook_controller.rb
@@ -1,5 +1,13 @@
 class MailgunWebhookController < ApplicationController
+  require 'csv'
+
+  protect_from_forgery except: :process_mailgun_webhook
 
   def process_mailgun_webhook
+    return unless ["clicked", "bounced"].include? params["event"]
+    CSV.open("webhook_data.csv", "a+") do |csv|
+      csv << [params["recipient"], params["ip"], params["ip"], params["event"]]
+    end
+    head 200
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
       }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#show"
+
+
+  post '/process_mailgun_webhook', to: 'mailgun_webhook#process_mailgun_webhook'
 end
